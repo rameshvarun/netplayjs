731eac4de31bf0bfe7b7930cae201afa64bee415
Varun Ramesh <varunramesh@live.com>
2023-03-21 20:21:09 -0700

    Print version number in docker publish script.

--- scripts/docker-push.js
+++ scripts/docker-push.js
@@ -2,6 +2,7 @@ const shell = require("shelljs");
 const path = require("path");
 
 const VERSION = require("../package.json").version;
+console.log(`Tagging and uploading version ${VERSION}...`)
 
 shell.exec("docker tag netplayjs-server:latest varunramesh/netplayjs-server:latest")
 shell.exec(`docker tag netplayjs-server:latest varunramesh/netplayjs-server:${VERSION}`)
