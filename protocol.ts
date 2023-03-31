c83c3953449b3f045332cb8146795edf70314ffb
Varun Ramesh <varunramesh@live.com>
2023-03-31 00:02:07 -0700

    Use Zod for validation

--- protocol.ts
+++ protocol.ts
@@ -1,81 +1,87 @@
+import { z } from "zod";
+
 // The version number of the matchmaking protocol.
 // This number is incremented whenever a backwards-incompatible change is made.
 export const PROTOCOL_VERSION = 1;
 
 /** The types of signaling messages one peer can send to another. */
-export type MessageType = "offer" | "answer" | "candidate";
+export const MessageType = z.union([
+  z.literal("offer"),
+  z.literal("answer"),
+  z.literal("candidate"),
+]);
 
 /** A message sent from a client to the server. */
-export type ClientMessage =
-  | {
-      /** Send a WebRTC signaling message to a peer. */
-      kind: "send-message";
-      destinationID: string;
-      type: MessageType;
-      payload: any;
-    }
-  | {
-      /** Request a list of ICE servers and their credentials. */
-      kind: "request-ice-servers";
-    }
-  | {
-      kind: "match-request";
+export const ClientMessage = z.union([
+  /** Send a WebRTC signaling message to a peer. */
+  z.object({
+    kind: z.literal("send-message"),
+    destinationID: z.string().uuid(),
+    type: MessageType,
+    payload: z.any(),
+  }),
+  /** Request a list of ICE servers and their credentials. */
+  z.object({
+    kind: z.literal("request-ice-servers"),
+  }),
+  /** Matchmaking request. */
+  z
+    .object({
+      kind: z.literal("match-request"),
+      gameID: z.string().nonempty(),
+      maxPlayers: z.number().min(2),
+      minPlayers: z.number().min(2),
+    })
+    .refine((req) => req.maxPlayers >= req.minPlayers),
+]);
 
-      /**
-       * A unique ID representing the game. You will
-       * only be matched to players with the same gameID.
-       */
-      gameID: string;
+export type ClientMessage = z.output<typeof ClientMessage>;
 
-      /** The maximum number of players a match can hold. */
-      maxPlayers: number;
+/** A message sent from the server to the client. */
+export const ServerMessage = z.union([
+  /** Sent as soon as a client connects in order to inform client of their ID. */
+  z.object({
+    kind: z.literal("registration-success"),
+    clientID: z.string().uuid(),
+  }),
+  /** An unexpected error occurred on the server side. The connection will be terminated. */
+  z.object({
+    kind: z.literal("server-error"),
+    reason: z.string().nonempty(),
+  }),
+  /** A signaling message failed to send. */
+  z.object({
+    kind: z.literal("send-message-failure"),
+    destinationID: z.string().uuid(),
+    reason: z.string().nonempty(),
+  }),
+  /** Deliver a signaling message from a peer. */
+  z.object({
+    kind: z.literal("peer-message"),
+    sourceID: z.string().uuid(),
+    type: MessageType,
+    payload: z.any(),
+  }),
+  /** Return a list of ICE servers and their credentials. */
+  z.object({
+    kind: z.literal("ice-servers"),
+    servers: z.any(),
+  }),
+  /** Tell the client it should host the match. */
+  z.object({
+    kind: z.literal("host-match"),
+    clientIDs: z.array(z.string().uuid()),
+  }),
+  /** Tell the client it should join a match hosted by another player. */
+  z.object({
+    kind: z.literal("join-match"),
+    hostID: z.string().uuid(),
+  }),
+  /** Tell the client it's match request failed. */
+  z.object({
+    kind: z.literal("match-request-failure"),
+    reason: z.string().nonempty(),
+  }),
+]);
 
-      /** The minimum number of players required for a match to start. */
-      minPlayers: number;
-    };
-
-// A message sent from the server to the client.
-export type ServerMessage =
-  | {
-      // Sent as soon as a client connects in order to inform client of their ID.
-      kind: "registration-success";
-      clientID: string;
-    }
-  | {
-      // An unexpected error occurred on the server side. The connection will be terminated.
-      kind: "server-error";
-      reason: string;
-    }
-  | {
-      // A signaling message failed to send.
-      kind: "send-message-failure";
-      destinationID: string;
-      reason: string;
-    }
-  | {
-      // Deliver a signaling message from a peer.
-      kind: "peer-message";
-      sourceID: string;
-      type: MessageType;
-      payload: any;
-    }
-  | {
-      // Return a list of ICE servers and their credentials.
-      kind: "ice-servers";
-      servers: any;
-    }
-  | {
-      /** Tell the client it should host the match. */
-      kind: "host-match";
-
-      /** The clients to expect connection requests from. */
-      clientIDs: Array<string>;
-    }
-  | {
-      kind: "join-match";
-      hostID: string;
-    }
-  | {
-      kind: "match-request-failure";
-      reason: string;
-    };
+export type ServerMessage = z.output<typeof ServerMessage>;
